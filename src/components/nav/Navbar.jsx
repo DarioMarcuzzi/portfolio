d9a2bef6ee2078ab0fbb22d12e1ac5804240c459
DarioMarcuzzi <darik95@hotmail.com>
2022-11-15 08:21:37 -0300

    navb

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='contenedor-nav-principal'>
+        <div className='logo-nav'>
+            <h1>logo</h1>
+        </div>
+        <div className='btn-links-nav'>
+          <Link to='/about'>
+            <button>Sobre nosotros</button>
+          </Link>
+          <Link to='/home'>
+            <button>Contacto</button>
+          </Link>
+        </div>
+        
     </div>
   );
 };
