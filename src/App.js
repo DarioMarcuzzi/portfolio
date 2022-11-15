9b0725a98219ddfe169d1c9f00dfccd42c5217ab
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-11-15 10:21:58 -0300

    landing a terminar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/about/About';
 import Landing from './components/landing/Landing';
 import PerfilDM from './components/perfilDM/PerfilDM';
 import PerfilDO from './components/perfilDO/PerfilDO';
-
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<About />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/perfilDM" element={<PerfilDM />} />
         <Route path="/perfilDO" element={<PerfilDO />} />
       </Routes>
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -6,13 +6,18 @@ const Navbar = () => {
   return (
     <div className='contenedor-nav-principal'>
         <div className='logo-nav'>
-            <h1>logo</h1>
+          <Link to='/'>
+            <img src='https://res.cloudinary.com/dcpcja2qg/image/upload/v1668455340/logoDD_logj6a.png' alt='Logo'  />
+          </Link>
         </div>
         <div className='btn-links-nav'>
           <Link to='/about'>
             <button>Sobre nosotros</button>
           </Link>
           <Link to='/home'>
+            <button>Menu</button>
+          </Link>
+          <Link to='/contact'>
             <button>Contacto</button>
           </Link>
         </div>
